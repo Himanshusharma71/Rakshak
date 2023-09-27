e4bfd0e9e0ca02c6fa60dd4e070a9ee64653035b
Himanshusharma71 <143833074+Himanshusharma71@users.noreply.github.com>
2023-09-27 10:29:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const logoutButton = document.getElementById("logoutButton");
+
+    // Add a click event listener for the logout button
+    logoutButton.addEventListener("click", function () {
+        // You can add your logout logic here
+        alert("Logging out..."); // Replace this with your actual logout code
+    });
+});
